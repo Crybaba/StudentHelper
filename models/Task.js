3c414dd41604c1662932e79a90b92fb0e0181e4f
Crybaby <goodegod2@gmail.com>
2024-08-01 18:35:15 +0800

    added is_personal field

--- models/Task.js
+++ models/Task.js
@@ -25,6 +25,11 @@ module.exports = (sequelize, DataTypes) => {
                 key: 'id'
             }
         },
+        is_personal: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
         group_id: {
             type: DataTypes.INTEGER,
             allowNull: true,
